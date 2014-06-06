d2d1896f2927d204ef59bdbf5c6660248e08ef79
luizrogeriocn <luizrogeriocn@gmail.com>
2014-06-06 02:38:10 -0300

    fix tabs and spaces. closes #18

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // BASE SETUP
 // ==============================================
 
-var express			= require('express');
-var bodyParser	= require('body-parser');
-var app     		= express();
-var port    		= process.env.PORT || 8080;
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 8080;
 var calculator = require('calculator');
 
 app.use(bodyParser());
